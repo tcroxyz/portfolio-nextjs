0ca068723f18cbf50396cd953bc1f5ff0db3bf73
tcroxyz <tom@tcro.xyz>
2022-06-01 00:34:51 +0100

    nav update & logo test

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100] bg-[#14b3d8]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Image
-          src="/../public/assets/navLogo.png"
+          src={`/assets/navLogo4.png`}
           alt="/"
-          width="125"
-          height="50"
+          width={175}
+          height={80}
+          quality={100}
         />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
       </div>
     </div>
   );
